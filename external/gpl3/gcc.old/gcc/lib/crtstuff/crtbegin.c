f9b88d0c5e6fec72456d6539e7ded5d8c1ec93fa
mrg <mrg@NetBSD.org>
2018-11-10 09:38:09 +0000

    import GCC 6.5 from gcc to gcc.old.  bombs away!
    this will likely be broken until merge is commited.

---
+++ external/gpl3/gcc.old/gcc/lib/crtstuff/crtbegin.c
@@ -0,0 +1,9 @@
+/*	$NetBSD: crtbegin.c,v 1.1.1.1 2018/11/10 09:39:16 mrg Exp $	*/
+
+/*
+ * Written by Jason R. Thorpe, August 24, 2004.
+ * Public domain.
+ */
+
+#define	CRT_BEGIN
+#include "crtstuff.c"
