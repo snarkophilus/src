832e465931ecadd65a050111d8220950a16eb6ce
Nick Hudson <skrll@netbsd.org>
2020-08-15 14:00:01 +0100

    Remove diff to upstream/trunk

--- sys/uvm/uvm_pager.c
+++ sys/uvm/uvm_pager.c
@@ -391,10 +391,8 @@ uvm_aio_aiodone_pages(struct vm_page **pgs, int npages, bool write, int error)
 #endif /* defined(VMSWAP) */
 
 		if (write && uobj != NULL) {
-			KASSERT(radix_tree_get_tag(&uobj->uo_pages,
-			    pg->offset >> PAGE_SHIFT, UVM_PAGE_WRITEBACK_TAG));
-			radix_tree_clear_tag(&uobj->uo_pages,
-			    pg->offset >> PAGE_SHIFT, UVM_PAGE_WRITEBACK_TAG);
+			KASSERT(uvm_obj_page_writeback_p(pg));
+			uvm_obj_page_clear_writeback(pg);
 		}
 
 		/*
