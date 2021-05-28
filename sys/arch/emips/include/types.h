8aebd9f680da8eabeb724dfa6ba4b5758ce71ae4
Nick Hudson <skrll@netbsd.org>
2021-05-28 06:46:39 +0100

    G/C

--- sys/arch/emips/include/types.h
+++ sys/arch/emips/include/types.h
@@ -4,8 +4,6 @@
 
 #define	__HAVE_DEVICE_REGISTER
 #define	__HAVE_GENERIC_SOFT_INTERRUPTS
-/* We'll use the FreeRunning counter everywhere */
-#define	__HAVE_TIMECOUNTER
 
 /* MIPS specific options */
 #define	__HAVE_BOOTINFO_H
